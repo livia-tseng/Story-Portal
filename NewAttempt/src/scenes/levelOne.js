28392041dd0473797b6631b879307dbca0792134
jayden-chao <jchao0217@gmail.com>
2024-11-13 19:32:58 -0800

    Update levelOne.js

--- NewAttempt/src/scenes/levelOne.js
+++ NewAttempt/src/scenes/levelOne.js
@@ -17,10 +17,10 @@ export class LevelOneScene extends Phaser.Scene {
     create(){
         const screenWidth = this.scale.width / this.textures.get('polarkidsleeping').getSourceImage().width;
         const screenHeight = this.scale.height / this.textures.get('polarkidsleeping').getSourceImage().height;
-        const newScale = Math.max(screenWidth, screenHeight)
-        this.add.image(0,0,'polarkidsleeping').setOrigin(0).setScale(newScale).setDepth(0);
+        
+        this.add.image(0,0,'polarkidsleeping').setOrigin(0).setScale(screenWidth,screenHeight).setDepth(0);
 
-        const sleep = this.add.text(120, 200,'theres supposed to be the kid sleeping but i scaled it wrong', {
+        const sleep = this.add.text(120, 200,'level one', {
                 fontFamily: 'BadComic-Regular',
                 color: 'white',
                 fontSize: '20px',
