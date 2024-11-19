8466e82917eaa3823e2a140fc662ca75310ebd7a
Samuel Wu <samuelsw@usc.edu>
2024-11-18 16:56:14 -0800

    back button for level one
    Added a back button for level one (using a container) so we ain't soft-locked no more

--- NewAttempt/src/scenes/levelOne.js
+++ NewAttempt/src/scenes/levelOne.js
@@ -26,6 +26,23 @@ export class LevelOneScene extends Phaser.Scene {
                 fontSize: '50px',
         }).setOrigin(0.5).setDepth(2);
 
+        //Back Button Stuff
+        const backButton = this.add.image(0, 0, 'mediumButton').setDepth(1);
+        const startText = this.add.text(0, 0, 'BACK',
+            {
+                fontFamily: 'BadComic-Regular',
+                color: '#D3D3D3',
+                fontSize: '60px',
+        }).setOrigin(0.5, 0.7).setDepth(2);
+        //Container!
+        const containerBackButton = this.add.container(backButton.width/2, backButton.height/2, [backButton, startText]);
+        containerBackButton.setSize(backButton.width, backButton.height);
+        containerBackButton.setInteractive();
+
+        containerBackButton.on("pointerup", ()=>{
+            this.scene.start(SCENE_KEYS.GAME_START_SCENE);
+        })
+
         
 }
 
