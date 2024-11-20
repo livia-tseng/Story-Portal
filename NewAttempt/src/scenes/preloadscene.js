4a0e3cc70642c0c47eec9632270270e468d8e64b
Samuel Wu <samuelsw@usc.edu>
2024-11-19 23:30:24 -0800

    Scene 1!
    Added in shake animation and transition

--- NewAttempt/src/scenes/preloadscene.js
+++ NewAttempt/src/scenes/preloadscene.js
@@ -23,15 +23,24 @@ export class PreloadScene extends Phaser.Scene {
     preload() {
         //this is how you load images (key/name/reference to image, path to actual image file)
         this.load.image('background', './assets/images/polarexpress.webp');
+<<<<<<< Updated upstream
         this.load.image('backgroundl2', './assets/images/level2bg.png');
         this.load.image('bgl3', './assets/images/level3bg.png');
+=======
+        this.load.image('bgl1','./assets/images/1-Background.png');
+        this.load.image('backgroundl2', './assets/images/background_level_two.png');
+        this.load.image('bgl3', './assets/images/level3bg.jpg');
+>>>>>>> Stashed changes
         this.load.image('bgl4', './assets/images/level4bg.png');
         this.load.image('mountainBackground', './assets/images/polar_express_mountain.jpg');
+        //Level 1 stuff
         this.load.image('samuelf','./assets/images/samuelf.png');
+        this.load.image('windowDark', './assets/images/1-windowDark.png');
+        this.load.image('windowLight', './assets/images/1-windowLight.png');
+
         this.load.image('jayden2', './assets/images/jayden2.jpg');
         this.load.image('jayden3', './assets/images/jayden3.jpg');
         this.load.image('ameen4', './assets/images/ameen4.jpg');
-        this.load.image('polarkidsleeping','./assets/images/polarkidsleeping.webp');
         this.load.image('startBtn', './assets/images/TextBTN_Big.png');
         this.load.image('creditsBtn', './assets/images/Exclamation_Yellow.png');
         this.load.image('grayExcl', './assets/images/Exclamation_Gray.png');
