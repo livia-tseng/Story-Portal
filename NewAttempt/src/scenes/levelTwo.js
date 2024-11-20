687387838e64e08be9c6e53d846f550c38e5bb42
Samuel Wu <samuelsw@usc.edu>
2024-11-19 17:58:41 -0800

    updated LevelTwo

--- NewAttempt/src/scenes/levelTwo.js
+++ NewAttempt/src/scenes/levelTwo.js
@@ -1,7 +1,7 @@
 import Phaser from '../lib/phaser.js';
 import { SCENE_KEYS } from './sceneKeys.js';
 
-//NEED TO IMPORT SCENE_KEYS AND MAIN MENU STUFF
+//NEED TO IMPORT SCENE_KEYS AND MAIN.JS
 export class LevelTwoScene extends Phaser.Scene {
     constructor() {
         super({
@@ -11,13 +11,50 @@ export class LevelTwoScene extends Phaser.Scene {
     }
 
     init() {
-        
+
     }
 
     create() {
         const screenWidth = 800 / this.textures.get('backgroundl2').getSourceImage().width;
         const screenHeight = 600 / this.textures.get('backgroundl2').getSourceImage().height;
         this.add.image(0,0, 'backgroundl2').setOrigin(0).setScale(screenWidth, screenHeight).setDepth(0);
+
+
+        //answer buttons
+        let buttonA = this.add.image(520, 80, 'jayden2').setOrigin(0.5).setScale(0.3,0.3).setDepth(1);
+        buttonA.setInteractive();
+        buttonA.on("pointerup", ()=> {
+            buttonA.setTint(0xFF0000).setAlpha(0.5)
+        })
+        let buttonB = this.add.image(680, 80, 'jayden2').setOrigin(0.5).setScale(0.3,0.3).setDepth(1);
+        buttonB.setInteractive();
+        buttonB.on("pointerup", ()=> {
+            buttonB.setTint(0xFF0000).setAlpha(0.5)
+        })
+
+        //Correct Answer
+        let buttonC = this.add.image(520, 200, 'jayden2').setOrigin(0.5).setScale(0.3,0.3).setDepth(1);
+        buttonC.setInteractive();
+        buttonC.on("pointerup", ()=> {
+            buttonC.setTint(0x00FF00).setAlpha(0.5)
+            let correctText = this.add.text(400,300,"Correct!", {
+                fontSize: "64px",
+                color:"#00ff00",
+                fontFamily: 'BadComic-Regular',
+            }).setOrigin(0.5);
+            this.time.delayedCall(1500, ()=> {
+                correctText.destroy();
+                this.scene.start(SCENE_KEYS.MAIN_MENU_SCENE);
+            })
+
+        })
+        
+
+        let buttonD = this.add.image(680, 200, 'jayden2').setOrigin(0.5).setScale(0.3,0.3).setDepth(1);
+        buttonD.setInteractive();
+        buttonD.on("pointerup", ()=> {
+            buttonD.setTint(0xFF0000).setAlpha(0.5)
+        })
 
         //Back Button Stuff
         const backButton = this.add.image(0, 0, 'mediumButton').setDepth(1);
